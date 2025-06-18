5235d324baf9e1dfcc334be5c83d15d457c79e7b
Vishal Bhalaje M <115567045+m-vishal-10@users.noreply.github.com>
2025-06-18 19:55:14 +0530

    Singleton Pattern Completed

---
+++ Engineering_concepts/Design_Patterns_and_Principles/FactoryMethod.java
@@ -0,0 +1,6 @@
+package Engineering_concepts.Design_Patterns_and_Principles;
+
+
+public class FactoryMethod {
+    
+}
---
+++ Engineering_concepts/Design_Patterns_and_Principles/SingletonPatternExample/LoggerTest.java
@@ -0,0 +1,21 @@
+package Engineering_concepts.Design_Patterns_and_Principles.SingletonPatternExample;
+
+// Main.java
+public class LoggerTest {
+    public static void main(String[] args) {
+        // Get singleton instance
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        // Use logger
+        logger1.log("First message");
+        logger2.log("Second message");
+
+        // Validate singleton behavior
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same (Singleton confirmed).");
+        } else {
+            System.out.println("Different instances found (Singleton failed).");
+        }
+    }
+}
