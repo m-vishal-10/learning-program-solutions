7b4e68288294821600455669ef6a3c29cd3817ba
Vishal Bhalaje M <115567045+m-vishal-10@users.noreply.github.com>
2025-07-25 14:33:56 +0530

    Week 4 5 6  Completed

---
+++ week06_react/handson02_react/handson02/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+
+
+function App() {
+
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
